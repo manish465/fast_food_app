5ffb57c0aed0122021d51d4bb47b983ff9f827ce
manish465 <ms5978307@gmail.com>
2021-04-12 02:03:50 -0700

    added sign out

---
+++ server/src/middlewere/requireSignin.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+const User = require("../models/auth");
+
+dotenv.config();
+
+module.exports = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(401).json({ err: "you must be logged in" });
+    }
+    const token = authorization.replace("Bearer ", "");
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+        if (err) {
+            return res.status(401).json({ err: "you must be logged in" });
+        }
+        const { _id } = payload;
+        User.findById(_id).then((user) => {
+            user.hash_password = undefined;
+            req.user = user;
+            next();
+        });
+    });
+};
--- server/src/router/auth.js
+++ server/src/router/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const routes = express.Router();
 
-const { signUp, signIn } = require("../controller/auth");
+const { signUp, signIn, signOut } = require("../controller/auth");
+const requireSignin = require("../middlewere/requireSignin");
 
 routes.post("/sign-in", signIn);
 routes.post("/sign-up", signUp);
+routes.get("/sign-out", requireSignin, signOut);
 
 module.exports = routes;
