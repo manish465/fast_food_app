f152b061ad5189bd7f415887632ba06370c49b7f
manish465 <ms5978307@gmail.com>
2021-04-29 09:09:32 -0700

    added total price

--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -1,12 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
-import axios from "axios";
 
 import { NavBar } from "../components";
 import { FlexColoumn, Padding, SizedBox } from "../styles";
 import { del } from "../assets/svg";
-import deafultIMG from "../assets/images/fast-food.jpg";
-import { useState } from "react";
+import { productContext } from "../context/product";
+import { picturebase } from "../adapter";
 
 const MainContainer = styled.div`
     background-color: ${({ theme }) => theme.colors.secondaryColor};
@@ -32,6 +31,7 @@ const OrderTab = styled.div`
 const OrderIMG = styled.img`
     width: 80px;
     height: 50px;
+    object-fit: contain;
 `;
 const OrderInfo = styled.h5`
     font-size: 1rem;
@@ -70,14 +70,17 @@ const CheckoutButton = styled.button`
 `;
 
 const Order = () => {
-    const [orderData, setOrderData] = useState([
-        { img: deafultIMG, title: "Cheeseburger1", multiple: 3, price: 23.45 },
-        { img: deafultIMG, title: "Cheeseburger2", multiple: 3, price: 23.45 },
-        { img: deafultIMG, title: "Cheeseburger3", multiple: 3, price: 23.45 },
-        { img: deafultIMG, title: "Cheeseburger4", multiple: 3, price: 23.45 },
-        { img: deafultIMG, title: "Cheeseburger5", multiple: 3, price: 23.45 },
-        { img: deafultIMG, title: "Cheeseburger6", multiple: 3, price: 23.45 },
-    ]);
+    const { productList, setProductList } = useContext(productContext);
+    const [totalPrice, setTotalPrice] = useState(0.0);
+
+    useEffect(() => {
+        setTotalPrice(
+            productList.reduce(
+                (total, product) => total + product.price * product.multiple,
+                0,
+            ),
+        );
+    }, [productList]);
 
     return (
         <>
@@ -87,28 +90,32 @@ const Order = () => {
                     <FlexColoumn>
                         <Heading>Your Order</Heading>
                         <SizedBox height='20px' />
-                        {orderData.map((data, key) => (
-                            <OrderTab key={key}>
-                                <OrderInfo>{key + 1}.</OrderInfo>
-                                <OrderIMG src={data.img} />
-                                <OrderInfo>{data.title}</OrderInfo>
-                                <OrderInfo>X{data.multiple}</OrderInfo>
-                                <OrderInfo>${data.price}</OrderInfo>
-                                <OrderDEL
-                                    src={del}
-                                    onClick={() =>
-                                        setOrderData((prevState) => {
-                                            let temp = [...prevState];
-                                            temp.splice(key, 1);
-                                            return temp;
-                                        })
-                                    }
-                                />
-                            </OrderTab>
-                        ))}
+                        {productList.length !== 0 ? (
+                            productList.map((data, key) => (
+                                <OrderTab key={key}>
+                                    <OrderInfo>{key + 1}.</OrderInfo>
+                                    <OrderIMG src={picturebase + data.img} />
+                                    <OrderInfo>{data.title}</OrderInfo>
+                                    <OrderInfo>X{data.multiple}</OrderInfo>
+                                    <OrderInfo>${data.price}</OrderInfo>
+                                    <OrderDEL
+                                        src={del}
+                                        onClick={() =>
+                                            setProductList((prevState) => {
+                                                let temp = [...prevState];
+                                                temp.splice(key, 1);
+                                                return temp;
+                                            })
+                                        }
+                                    />
+                                </OrderTab>
+                            ))
+                        ) : (
+                            <>ADD AN ITEM</>
+                        )}
                         <SizedBox height='20px' />
                         <Checkout>
-                            <CheckoutPrice>$88.56</CheckoutPrice>
+                            <CheckoutPrice>${totalPrice}</CheckoutPrice>
                             <CheckoutButton>CHECKOUT</CheckoutButton>
                         </Checkout>
                     </FlexColoumn>
